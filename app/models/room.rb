3c8b161f80f07f7e41f1235988ac85bbb6182440
Ukaypromise <promiseukaimo@gmail.com>
2023-06-18 20:24:38 +0100

    Add messagesController

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+    has_many :messages, dependent: :destroy
+    # has_many :users, through: :messages
+    
+    validates :name, presence: true, uniqueness: true
 end
