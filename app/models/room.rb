e3ea8cd8456b723e2f98c01704653743cb712b7f
Ukaypromise <promiseukaimo@gmail.com>
2023-06-18 21:33:08 +0100

    Add turbo-frame-tag

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,5 @@ class Room < ApplicationRecord
     has_many :messages, dependent: :destroy
     # has_many :users, through: :messages
     
-    validates :name, presence: true, uniqueness: true
+    # validates :name, presence: true, uniqueness: true
 end
