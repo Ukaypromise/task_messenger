d641d3fa8d530ced1fcd38f026b8d892c5a74b09
Ukaypromise <promiseukaimo@gmail.com>
2023-06-20 20:21:32 +0100

    Complete tobo-drive setup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
+  post "tasks/:id/toggle", to: "tasks#toggle"
 
   # Defines the root path route ("/")
   root "tasks#index"
